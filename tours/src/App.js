05b889c3c9c7f443f596ce74aff1fd98ea63cd52
atakantekoglu <atakantekoglu@gmail.com>
2022-04-24 16:01:17 +0300

    fetch data and scaffold index page

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import Loading from './components/Loading'
+import Tours from './components/Tours'
 
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [tours, setTours] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchData = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setTours(data)
+    setIsLoading(false)
+  }
+
+  useEffect(()=> {
+    fetchData()
+  }, [])
+
+  if (isLoading) {
+    return <main><Loading /></main>
+  } else {
+    return (
+      <main>
+        <Tours tours={tours} />
+      </main>
+    )
+  }
 }
 
 export default App
