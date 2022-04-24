6f7f3df96df3aac67ba11394e0009d425ada64bc
atakantekoglu <atakantekoglu@gmail.com>
2022-04-24 19:34:32 +0300

    grocery project done

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -3,7 +3,78 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, message: '', type: '' })
+
+  const handleSubmit = e => {
+      e.preventDefault()
+      if(!name) {
+          showAlert(true,'Please enter a name','danger' )
+          return
+      } else if(name && isEditing) {
+          setAlert({ show: true, message: 'Success', type: 'danger' })
+          setList(list.map(item => item.id === editID ? { id: editID, name: name } : item))
+          setIsEditing(false)
+          setEditID(null)
+          setAlert({ show: false, message: '', type: '' })
+          setName('')
+          return
+      } else {
+          const newGrocery = {
+              id: Date.now(),
+              name: name,
+          }
+          setList((item) => {
+              return [...item, newGrocery]
+          })
+          setAlert({ show: true, message: 'Grocery added', type: 'success' })
+          setName('')
+      }
+  }
+
+  const showAlert = (show= false, message = '', type = '') => {
+    setAlert({ show, message, type })
+  }
+
+  const removeItem = (id) => {
+      setAlert({ show: true, message: 'Grocery removed', type: 'success' })
+      const newList = list.filter(item => item.id !== id)
+      setList(newList)
+  }
+  const editItem = (id) => {
+      const targetItem = list.find(item => item.id == id)
+      setName(targetItem.name)
+      setIsEditing(true)
+      setEditID(id)
+  }
+
+  return (
+      <section className='section-center'>
+        <form className='grocery-form' onSubmit={handleSubmit}>
+            {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+            <h3>grocery bud</h3>
+            <div className='form-control'>
+                <input type='text'
+                       className='grocery'
+                       id='name'
+                       value={name}
+                       placeholder='add grocery item'
+                       onChange={e => setName(e.target.value)} />
+                <button type='submit'
+                        className='submit-btn'>
+                    {isEditing ? 'edit' : 'submit'}
+                </button>
+            </div>
+        </form>
+        <div className='grocery-container'>
+          <List list={list} editItem={editItem}  removeItem={removeItem}/>
+          <button className='clear-btn' onClick={() => setList([])}>{list.length>0 ? 'clear items' : 'no items'}</button>
+        </div>
+      </section>
+  )
 }
 
 export default App
--- grocery-bud/src/List.js
+++ grocery-bud/src/List.js
@@ -1,7 +1,34 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-    return <h2>list component</h2>
+const List = ({list, removeItem, editItem}) => {
+    return (
+        <div className='grocery-list'>
+            {list.map((item) => {
+                const { id, name } = item;
+                return (
+                    <article className='grocery-item' key={id}>
+                        <p className='title'>{name}</p>
+                        <div className='btn-container'>
+                            <button
+                                type='button'
+                                className='edit-btn'
+                                onClick={() => editItem(id)}
+                            >
+                                <FaEdit />
+                            </button>
+                            <button
+                                type='button'
+                                className='delete-btn'
+                                onClick={() => removeItem(id)}
+                            >
+                                <FaTrash />
+                            </button>
+                        </div>
+                    </article>
+                );
+            })}
+        </div>
+    );
 }
 
 export default List
